5c87e8a23f4b51c777c73d35845eea11f2b95fc7
sadbut_1 <eduardommoya@gmail.com>
2017-12-05 14:25:38 -0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            deadline: 'December 25, 2017'
+        }
+    }
+    render() {
+        return (
+            <div className="App">
+                <div className="App-title">Countdown to {this.state.deadline}</div>
+                <div>
+                    <div className="Clock-days">14 days</div>
+                    <div className="Clock-hours">30 hours</div>
+                    <div className="Clock-minutes">15 minutes</div>
+                    <div className="Clock-seconds">20 seconds</div>
+                </div>
+                <div>
+                    <input placeholder="Nova data" />
+                    <button>Enviar</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App;
